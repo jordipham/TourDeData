393c37ef14f3dee015dd7462b0d4529a57bf5aab
jordipham <jop037@ucsd.edu>
2025-05-15 19:53:25 -0700

    Updated mapjs with bike routes for Boston

--- map.js
+++ map.js
@@ -5,7 +5,8 @@ import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
 console.log("Mapbox GL JS Loaded:", mapboxgl);
 
 // Set your Mapbox access token here
-mapboxgl.accessToken = "pk.eyJ1Ijoiam9yZGlwaGFtIiwiYSI6ImNtYXB2NXUwMTAydzAya3B1N2N5dXpwZHcifQ.oxgdaI-UtftkxIn-p32hAQ";
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9yZGlwaGFtIiwiYSI6ImNtYXB2NXUwMTAydzAya3B1N2N5dXpwZHcifQ.oxgdaI-UtftkxIn-p32hAQ";
 
 // Initialize the map
 const map = new mapboxgl.Map({
@@ -15,4 +16,24 @@ const map = new mapboxgl.Map({
   zoom: 12, // Initial zoom level
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
+});
+
+map.on("load", async () => {
+  // Add the data source
+  map.addSource("boston_route", {
+    type: "geojson",
+    data: "https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson",
+  });
+
+  // Add the layer to visualize the data
+  map.addLayer({
+    id: "bike-lanes",
+    type: "line",
+    source: "boston_route",
+    paint: {
+      "line-color": "#32D400", // control color of lines on the map
+      "line-width": 4, // control line width
+      "line-opacity": 0.5, // control transparency
+    },
+  });
 });
