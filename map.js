7abce25a378a45549054ac460145fa52fb7899e3
jordipham <jop037@ucsd.edu>
2025-05-15 19:40:18 -0700

    Created the map onto the site

--- map.js
+++ map.js
@@ -1,5 +1,18 @@
 // Import Mapbox as an ESM module
-import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import mapboxgl from "https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm";
 
 // Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
+console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = "pk.eyJ1Ijoiam9yZGlwaGFtIiwiYSI6ImNtYXB2NXUwMTAydzAya3B1N2N5dXpwZHcifQ.oxgdaI-UtftkxIn-p32hAQ";
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: "map", // ID of the div where the map will render
+  style: "mapbox://styles/mapbox/streets-v12", // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
